20a0791cbc66d766ac84baf20e8f6961a32e9c65
Francisco Ventura <franciscoduarteventura@gmail.com>
2015-05-08 14:55:17 +0100

    Estrategia 4 completa para submarinos

--- aux_tabuleiro.c
+++ aux_tabuleiro.c
@@ -412,7 +412,7 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 	int j;
 	int i;
 
-	//substitui unknowns ('o') da primeira linha (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~' 
+	/*substitui unknowns ('o') da primeira linha (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~'*/ 
 	j=0;
 	for (i=1; i<((estado->n_colunas)-1); i++){
 		if ((estado->tabuleiro[i][j] == 'o') && (estado->tabuleiro[i-1][j] == '~') && (estado->tabuleiro[i+1][j] == '~') && (estado->tabuleiro[i][j+1] == '~') ){
@@ -420,7 +420,7 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 		}
 	}
 
-	//substitui unknowns ('o') da última linha (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~' 
+	/*substitui unknowns ('o') da última linha (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~'*/
 	j=(estado->n_linhas)-1;
 
 	for (i=1; i<((estado->n_colunas)-1); i++){
@@ -429,7 +429,7 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 		}
 	}
 
-	//substitui unknowns ('o') da primeira coluna (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~' 
+	/*substitui unknowns ('o') da primeira coluna (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~'*/
 	i=0;
 	for (j=1; j<((estado->n_linhas)-1); j++){
 		if ((estado->tabuleiro[i][j] == 'o') && (estado->tabuleiro[i][j-1] == '~') && (estado->tabuleiro[i][j+1] == '~') && (estado->tabuleiro[i+1][j] == '~') ){
@@ -437,7 +437,7 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 		}
 	}
 
-	//substitui unknowns ('o') da primeira coluna (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~' 
+	/*substitui unknowns ('o') da primeira coluna (excepto cantos) do tabuleiro por submarinos, se à volta for tudo '~' */
 	i=(estado->n_colunas)-1;
 
 	for (j=1; j<((estado->n_linhas)-1); j++){
@@ -446,9 +446,7 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 		}
 	}
 
-	//substitui unknowns ('o') dos cantos por submarinos, se à volta for tudo '~'
-	i=0;
-	j=0;
+	/*substitui unknowns ('o') dos cantos por submarinos, se à volta for tudo '~'*/
 	if (estado->tabuleiro[0][0]=='o' && estado->tabuleiro[0][1]=='~' && estado->tabuleiro[1][0]=='~')
 		altera_estado(estado, 0, 0, 'O', partida);
 
@@ -461,7 +459,14 @@ void from_unknown_to_submarine(TAB_BN *estado, STACK *partida){
 	if (estado->tabuleiro[estado->n_colunas-1][estado->n_linhas-1]=='o' && estado->tabuleiro[estado->n_colunas-1][estado->n_linhas-2]=='~' && estado->tabuleiro[estado->n_colunas-2][estado->n_linhas-1]=='~')
 		altera_estado(estado, (estado->n_colunas-1), (estado->n_linhas-1), 'O', partida);
 
-
+	/*substitui unknowns ('o') do meio do tabuleiro por submarinos, se à volta for tudo '~'*/
+	for (i=1; i<(estado->n_colunas-2); i++){
+		for (j=1; j<(estado->n_linhas-2); j++){
+			if (estado->tabuleiro[i][j] == 'o' && estado->tabuleiro[i-1][j] == '~' && estado->tabuleiro[i+1][j]=='~' && estado->tabuleiro[i][j+1]=='~' && estado->tabuleiro[i][j-1]=='~'){
+				altera_estado(estado, i, j, 'O', partida);
+			}
+		}
+	}
 }
 
 
